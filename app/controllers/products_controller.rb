55fa001eb6279ca12abe675b6a12be6c4f73adfc
safiy cham <csafi13@yahoo.com>
2017-12-01 14:30:49 -0800

    add Category filtering function on index page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,12 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all.order('created_at DESC')
+    if params[:category].blank?
+      @products = Product.all.order('created_at DESC')
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @products = Product.where(:category_id => @category_id).order('created_at DESC')
+    end
   end
 
   def show
