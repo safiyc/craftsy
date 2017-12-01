1637fde70680457d6affbcfab3b5417cd416aa4a
safiy cham <csafi13@yahoo.com>
2017-12-01 11:26:48 -0800

    add Products edit and delete controller functions, and views files show, edit.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,22 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def product_params
---
+++ app/views/products/edit.html.erb
@@ -0,0 +1,1 @@
+<h3>Edit Product</h3>
