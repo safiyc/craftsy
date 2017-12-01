774c7c88b0741dac85cb13a5bd9b963ae2d9978c
safiy cham <csafi13@yahoo.com>
2017-12-01 10:41:39 -0800

    add Product new and create controller functions with views new and _form partial files.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,18 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :desc, :price)
+  end
 end
