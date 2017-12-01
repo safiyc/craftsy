878e1c05a255ea0bc62f118e4a408ef90426dcca
safiy cham <csafi13@yahoo.com>
2017-12-01 10:57:39 -0800

    add Products index and show controller functions with views files index, show.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
+    @products = Product.all.order('created_at DESC')
+  end
+
+  def show
   end
 
   def new
@@ -9,11 +14,21 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :desc, :price)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
