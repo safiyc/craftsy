608aea8d8d350df4b78a454346556c47fc311386
safiy cham <csafi13@yahoo.com>
2017-12-01 12:09:03 -0800

    add navbar and user_id association to products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,11 +9,11 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @product = current_user.product.build
   end
 
   def create
-    @product = Product.new(product_params)
+    @product = current_user.product.build(product_params)
 
     if @product.save
       redirect_to root_path
