2b6ab9756757bf43799777e75c775dff484e6fa2
safiy cham <csafi13@yahoo.com>
2017-12-01 13:38:50 -0800

    add Category model and assign relations with Products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  
+  belongs_to :category
+
   validates :name, :desc, :price, :presence => true
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  factory :category do
+    name "MyString"
+  end
   factory :user do
     
   end
