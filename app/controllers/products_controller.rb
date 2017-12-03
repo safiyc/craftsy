a3383dfb40b3f522f8cf029a266242e95a66109d
Safiy Cham <csafi13@yahoo.com>
2017-12-03 11:40:17 -0800

    add user authentication to prevent non users to create, edit and delete products and reviews.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     if params[:category].blank?
